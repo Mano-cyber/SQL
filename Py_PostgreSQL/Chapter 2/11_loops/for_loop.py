903d25a74627a117d988b8c8793e71f190d9ac8f
Mano Mambane <manomambane05@gmail.com>
2021-07-29 22:19:37 +0200

    Code

--- Py_PostgreSQL/Chapter 2/11_loops/for_loop.py
+++
@@ -1,7 +0,0 @@
-friends = ["Rolf", "Jen" ,"Bob", "Anne"]
-
-for friend in friends:
-    print(f"{friend} is my friend.")
-
-for friend in range(4):
-    print(f"{friend} is my friend.")
--- Py_PostgreSQL/Chapter 2/13_dictionaries/friend_ages.py
+++
@@ -1,9 +0,0 @@
-friend_ages = {"Rolf": 24, "Adam": 30, "Anne": 27}
-
-#add
-friend_ages["Bob"] = 20
-
-#change
-friend_ages["Rolf"] = 20
-
-print(friend_ages)
