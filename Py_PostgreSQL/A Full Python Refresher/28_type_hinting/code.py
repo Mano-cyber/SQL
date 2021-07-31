757fe54601abe2bc5af233839c622cfa97ce2c6b
Mano Mambane <manomambane05@gmail.com>
2021-07-31 13:21:02 +0200

    Code

---
+++ Py_PostgreSQL/A Full Python Refresher/28_type_hinting/code.py
@@ -0,0 +1,21 @@
+class Book:
+    TYPES = ("hardcover", "paperback")
+
+    def __init__(self, name: str, book_type: str, weight: int):
+        self.name = name
+        self.book_type = book_type
+        self.weight = weight
+
+    def __repr___(self) -> str:
+        return f"<Book {self.name}, {self.book_type}, it's weight is {self.weight}g>"
+    
+    def __str__(self):
+        return f"Book {self.name}, {self.book_type}, weighing {self.weight}g"
+
+    @classmethod
+    def hardcover(cls, name: str, page_weight: int) -> "Book":
+        return cls(name, cls.TYPES[0], page_weight + 100)
+
+    @classmethod
+    def paperback(cls, name: str, page_weight: int) -> "Book":
+        return cls(name, cls.TYPES[1], page_weight)
